4ce4e581e2428b51632645205430ecabd6ac7d74
thatalloguy <allosander@gmail.com>
2024-05-22 12:44:04 +0200

    ECS v1

--- Runtime/include/world/query.h
+++ Runtime/include/world/query.h
@@ -11,7 +11,80 @@ namespace catos {
 
 
     template<typename... Components>
-    struct Query {};
+    struct Query {
+
+
+        Query(World& world) : p_world(&world) {
+
+            if (sizeof...(Components) == 0) {
+                all = true;
+            } else {
+                //unpacking time :)
+                int component_ids[] = {0, Component::get_id<Components>() ...};
+                for (int i = 1; i < (sizeof...(Components) + 1); i++) {
+                    componentMask.set(component_ids[i], true);
+                }
+            }
+
+        };
+
+        struct Iterator {
+
+            Iterator(World* world, EntityIndex index, ComponentMask mask, bool all) :
+             p_world(world), index(index), mask(mask), all(all) {}
+
+            EntityId operator*() const {
+                return p_world->entities[index].id;
+            };
+
+            bool operator==(const Iterator& other) const {
+                return index == other.index || index == p_world->entities.size();
+            };
+
+            bool operator!=(const Iterator& other) const {
+                return index != other.index && index != p_world->entities.size();
+            };
+            bool valid_index() {
+                return EntityUtils::is_entity_valid(p_world->entities[index].id) && (all || mask == (mask & p_world->entities[index].mask));
+            }
+
+            Iterator& operator++() {
+                do {
+                    index++;
+                } while (index < p_world->entities.size() && !valid_index());
+                return *this;
+            }
+
+            EntityIndex index ;
+            World* p_world;
+            ComponentMask  mask;
+            bool all{false};
+
+        };
+
+        const Iterator begin() const {
+            int firstIndex = 0;
+            while (firstIndex < p_world->entities.size() &&
+                   (componentMask != (componentMask & p_world->entities[firstIndex].mask)
+                    || !EntityUtils::is_entity_valid(p_world->entities[firstIndex].id)))
+            {
+                firstIndex++;
+            }
+
+            return Iterator(p_world, firstIndex, componentMask, all);
+        };
+        const Iterator end() const {
+            return Iterator(p_world, EntityIndex(p_world->entities.size()), componentMask, all);
+        };
+
+
+
+        World* p_world{nullptr};
+        ComponentMask  componentMask;
+        bool all {false};
+
+    };
+
 
 
 }
--- Runtime/src/world/world.cpp
+++ Runtime/src/world/world.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "world/world.h"
+
+catos::EntityId catos::World::new_entity() {
+
+
+    if (!free_entities.empty()) {
+        EntityIndex new_index = free_entities.back();
+        free_entities.pop_back();
+        EntityId new_id = EntityUtils::create_entity_id(new_index, EntityUtils::get_entity_version(entities[new_index].id));
+        entities[new_index].id = new_id;
+        return entities[new_index].id;
+    }
+    entities.push_back({EntityUtils::create_entity_id(EntityIndex(entities.size()), 0), ComponentMask() });
+    return entities.back().id;
+}
+
+
+
