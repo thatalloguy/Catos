12a67366dddf53079708ccf3db544369ca2455d5
thatalloguy <allosander@gmail.com>
2024-08-22 11:56:34 +0200

    small

--- Runtime/src/stl/string.h
+++ Runtime/src/stl/string.h
@@ -7,40 +7,40 @@
 
 namespace STL {
 
-    // Custom string class that functions as dynamic C_str.
+    /// Custom string class that functions as dynamic C_str.
     class String {
 
     public:
 
-        // Initializes the internal buff to nullptr and size to 0.
+        /// Initializes the internal buff to nullptr and size to 0.
         String() : buf(nullptr), size(0) {};
 
-        //Copies the char array to the internal buffer.
+        /// Copies the char array to the internal buffer.
         String(const char* buffer);
 
-        // Copies string object to its internal values.
+        /// Copies string object to its internal values.
         String(const String& obj);
 
-        // Transfers ownerShip of buffers to this.
+        /// Transfers ownerShip of buffers to this.
         String(String&& obj);
 
-        // Same as the String(const String& obj) constructor.
+        /// Same as the String(const String& obj) constructor.
         String& operator=(const String& obj);
 
-        // Transfers ownership from a buffer to this.
+        /// Transfers ownership from a buffer to this.
         String& operator=(String&& obj);
 
-        //Adds a string to itself.
+        /// Adds a string to itself.
         String operator+(const String& obj);
 
-        // Returns the size of the internal buffer.
+        /// Returns the size of the internal buffer.
         unsigned int length() { return size; };
 
-        // Returns the internal buffer;
+        /// Returns the internal buffer;
         const char* c_str() const { return buf; };
 
 
-        // Deletes the allocated char array
+        /// Deletes the allocated char array
         ~String();
 
     private:
