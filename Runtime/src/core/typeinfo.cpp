688df7946b77f431864569f6cddd1bdae2c17b1e
thatalloguy <allosander@gmail.com>
2025-07-27 19:04:42 +0200

    TypeInfo improvement + stability

--- Runtime/src/core/typeinfo.cpp
+++ Runtime/src/core/typeinfo.cpp
@@ -8,11 +8,27 @@
 
 catos::TypeInfo &catos::TypeInfo::inherits(const std::string &class_name) {
     auto reg =  Registry::get();
+
+    if (!reg.is_type_registered(class_name)) {
+        spdlog::error(
+            "Could not inherit {} from {} , because the class isnt registered/found",
+            this->name.c_str(),
+            class_name.c_str()
+        );
+
+        return *this;
+    }
     auto p = reg.get_type(class_name);
 
     for (auto prop : p.properties) {
         properties.insert(prop);
     }
+
+    for (auto meths : p.methods) {
+        methods.insert(meths);
+    }
+
+    p.children.push_back(this);
 
     return *this;
 }
